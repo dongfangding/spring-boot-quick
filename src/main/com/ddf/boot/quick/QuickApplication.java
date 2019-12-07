c673fe6ea89e539506f65d994ec069917fc64584
dongfang.ding <1041765757@qq.com>
2019-12-08 00:38:10 +0800

    init

---
+++ src/main/com/ddf/boot/quick/QuickApplication.java
@@ -0,0 +1,15 @@
+package com.ddf.boot.quick;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ *
+ * @author dongfang.ding
+ * @date 2019/12/7 0007 23:28
+ */
+public class QuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickApplication.class, args);
+    }
+}
