242b29bd5dd9940d8c8aac1bdda4b594f0c43d51
dongfang.ding <1041765757@qq.com>
2019-12-08 22:48:25 +0800

    项目初配置

---
+++ src/main/com/ddf/boot/quick/mapper/AuthUserMapper.java
@@ -0,0 +1,15 @@
+package com.ddf.boot.quick.mapper;
+
+
+import com.ddf.common.model.datao.quick.AuthUser;
+import com.ddf.common.mybatis.mapper.CustomizeBaseMapper;
+
+/**
+ * 用户映射类
+ *
+ * @author dongfang.ding
+ * @date 2019/5/21 9:56
+ */
+public interface AuthUserMapper extends CustomizeBaseMapper<AuthUser> {
+
+}
