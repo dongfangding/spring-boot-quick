abda1547aec2eefd01840ee7368392166d859c3a
dongfang.ding <1041765757@qq.com>
2019-12-12 22:52:03 +0800

    update

--- src/main/com/ddf/boot/quick/biz/AsyncTask.java
+++ src/main/com/ddf/boot/quick/biz/AsyncTask.java
@@ -1,6 +1,7 @@
 package com.ddf.boot.quick.biz;
 
 import com.ddf.boot.common.mq.definition.QueueBuilder;
+import com.ddf.boot.common.mq.exception.MqSendException;
 import com.ddf.boot.common.mq.helper.RabbitTemplateHelper;
 import com.ddf.boot.quick.model.dto.LogUserLoginHistoryDto;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +23,19 @@ public class AsyncTask {
         if (userLoginHistoryDto == null) {
             log.error("用户登录日志数据不全！");
         }
-        RabbitTemplateHelper.wrapperAndSend(QueueBuilder.QueueDefinition.USER_LOGIN_HISTORY_QUEUE, userLoginHistoryDto);
+        try {
+            RabbitTemplateHelper.wrapperAndSend(QueueBuilder.QueueDefinition.USER_LOGIN_HISTORY_QUEUE, userLoginHistoryDto);
+        } catch (MqSendException ignored) {}
+
+        try {
+            // 发送给一个死信队列
+            RabbitTemplateHelper.wrapperAndSend(QueueBuilder.QueueDefinition.TEST_DEAD_LETTER_QUEUE, userLoginHistoryDto );
+        } catch (MqSendException ignored) {}
+
+        try {
+            // 发送给一个死信延迟队列
+            RabbitTemplateHelper.wrapperAndSend(QueueBuilder.QueueDefinition.TEST_TTL_QUEUE, userLoginHistoryDto);
+        } catch (MqSendException ignored) {}
     }
 
 
