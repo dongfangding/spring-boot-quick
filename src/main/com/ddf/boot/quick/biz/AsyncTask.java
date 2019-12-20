918c9f8b7ca5f476960406d5d403b4abb7c9350e
dongfang.ding <1041765757@qq.com>
2019-12-20 15:57:49 +0800

    update

--- src/main/com/ddf/boot/quick/biz/AsyncTask.java
+++ src/main/com/ddf/boot/quick/biz/AsyncTask.java
@@ -5,6 +5,7 @@ import com.ddf.boot.common.mq.exception.MqSendException;
 import com.ddf.boot.common.mq.helper.RabbitTemplateHelper;
 import com.ddf.boot.quick.model.dto.LogUserLoginHistoryDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -18,24 +19,28 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AsyncTask {
 
+
+    @Autowired
+    private RabbitTemplateHelper rabbitTemplateHelper;
+
     @Async("userLoginLogExecutor")
     public void logUserLoginHistory(LogUserLoginHistoryDto userLoginHistoryDto) {
         if (userLoginHistoryDto == null) {
             log.error("用户登录日志数据不全！");
         }
         try {
-            RabbitTemplateHelper.wrapperAndSend(QueueBuilder.QueueDefinition.USER_LOGIN_HISTORY_QUEUE, userLoginHistoryDto);
+            rabbitTemplateHelper.wrapperAndSend(QueueBuilder.QueueDefinition.USER_LOGIN_HISTORY_QUEUE, userLoginHistoryDto);
         } catch (MqSendException ignored) {}
 
-        try {
-            // 发送给一个死信队列
-            RabbitTemplateHelper.wrapperAndSend(QueueBuilder.QueueDefinition.TEST_DEAD_LETTER_QUEUE, userLoginHistoryDto );
-        } catch (MqSendException ignored) {}
-
-        try {
-            // 发送给一个死信延迟队列
-            RabbitTemplateHelper.wrapperAndSend(QueueBuilder.QueueDefinition.TEST_TTL_QUEUE, userLoginHistoryDto);
-        } catch (MqSendException ignored) {}
+//        try {
+//            // 发送给一个死信队列
+//            rabbitTemplateHelper.wrapperAndSend(QueueBuilder.QueueDefinition.TEST_DEAD_LETTER_QUEUE, userLoginHistoryDto );
+//        } catch (MqSendException ignored) {}
+//
+//        try {
+//            // 发送给一个死信延迟队列
+//            rabbitTemplateHelper.wrapperAndSend(QueueBuilder.QueueDefinition.TEST_TTL_QUEUE, userLoginHistoryDto);
+//        } catch (MqSendException ignored) {}
     }
 
 
