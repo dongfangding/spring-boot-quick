8022a3eb2f41ade098b480cd0910c5e41a4a76e8
dongfang.ding <1041765757@qq.com>
2020-09-21 19:23:17 +0800

    提供mongo通用分页工具类

--- src/main/java/com/ddf/boot/quick/controller/UserLoginHistoryController.java
+++ src/main/java/com/ddf/boot/quick/controller/UserLoginHistoryController.java
@@ -1,22 +1,18 @@
 package com.ddf.boot.quick.controller;
 
+import com.ddf.boot.mongo.helper.MongoTemplateHelper;
 import com.ddf.boot.quick.model.bo.PageUserHistoryBo;
 import com.ddf.boot.quick.mongo.collection.UserLoginHistoryCollection;
 import io.swagger.annotations.Api;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * 用户登录日志控制器$
@@ -29,7 +25,7 @@ import java.util.List;
 @Api(value = "用户登录日志控制器$", tags = {"用户登录日志控制器$"})
 public class UserLoginHistoryController {
     @Autowired
-    private MongoTemplate mongoTemplate;
+    private MongoTemplateHelper mongoTemplateHelper;
 
     /**
      * 分页查询用户登录日志
@@ -44,13 +40,6 @@ public class UserLoginHistoryController {
         if (pageUserHistoryBo.getUserId() != null) {
             query.addCriteria(Criteria.where("userId").is(pageUserHistoryBo.getUserId()));
         }
-        long count = mongoTemplate.count(query, UserLoginHistoryCollection.class);
-        if (count > 0) {
-            Pageable pageable = pageUserHistoryBo.ofSpringData();
-            query.with(pageable);
-            List<UserLoginHistoryCollection> list = mongoTemplate.find(query, UserLoginHistoryCollection.class);
-            return new PageImpl<>(list, pageable, count);
-        }
-        return Page.empty();
+        return mongoTemplateHelper.handlerPage(pageUserHistoryBo, query, UserLoginHistoryCollection.class);
     }
 }
