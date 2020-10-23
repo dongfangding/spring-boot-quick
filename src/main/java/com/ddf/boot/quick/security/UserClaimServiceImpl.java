b9b04ea85e3a6972651f327b3d24e965742e79d6
dongfangding <1041765757@qq.com>
2020-10-23 23:31:44 +0800

    fix

--- src/main/java/com/ddf/boot/quick/security/UserClaimServiceImpl.java
+++ src/main/java/com/ddf/boot/quick/security/UserClaimServiceImpl.java
@@ -52,7 +52,7 @@ public class UserClaimServiceImpl implements UserClaimService {
      */
     @Override
     public UserClaim getStoreUserInfo(UserClaim userClaim) {
-        UserClaim<?> dbUserClaim = new UserClaim<>();
+        UserClaim dbUserClaim = new UserClaim();
         AuthUser dbUser = authUserService.getById(userClaim.getUserId());
         // 将当前token对应的用户查询出来返回，给调用方用户将数据库数据和token进行比较
         dbUserClaim.setUserId(Convert.toStr(dbUser.getId()))
