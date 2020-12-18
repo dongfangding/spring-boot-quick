52fadb6ad9f48f1c2ccbfb43cc707bd969bd24bb
dongfangding <1041765757@qq.com>
2020-12-18 23:45:05 +0800

    基于redisson使用基于漏桶算法的分布式限流

--- src/main/java/com/ddf/boot/quick/controller/RedissonController.java
+++ src/main/java/com/ddf/boot/quick/controller/RedissonController.java
@@ -2,13 +2,18 @@ package com.ddf.boot.quick.controller;
 
 import com.ddf.boot.common.core.exception200.GlobalCallbackCode;
 import com.ddf.boot.common.redis.helper.RedisTemplateHelper;
-import com.ddf.boot.quick.common.redis.RedisRequestDefinition;
 import com.google.common.base.Preconditions;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RRateLimiter;
+import org.redisson.api.RateIntervalUnit;
+import org.redisson.api.RateType;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Redisson相关$
@@ -23,14 +28,27 @@ public class RedissonController {
 
     private final RedisTemplateHelper redisTemplateHelper;
 
+    private final RedissonClient redissonClient;
+
+    private static RRateLimiter limiter;
+
+    @PostConstruct
+    public void init() {
+        limiter = redissonClient.getRateLimiter("myLimiter");
+        limiter.trySetRate(RateType.OVERALL, 5, 1, RateIntervalUnit.SECONDS);
+    }
+
+
+
     /**
      * 演示基于漏桶算法的分布式限流
      * @return
      */
     @PostMapping("testLeakyBucketRateLimitA")
     public Boolean testLeakyBucketRateLimitA() {
-        Preconditions.checkArgument(redisTemplateHelper.leakyBucketRateLimitAcquire(RedisRequestDefinition.testLeakyBucketRateLimitA),
-                GlobalCallbackCode.RATE_LIMIT);
+//        Preconditions.checkArgument(redisTemplateHelper.leakyBucketRateLimitAcquire(RedisRequestDefinition.testLeakyBucketRateLimitA),
+//                GlobalCallbackCode.RATE_LIMIT);
+        Preconditions.checkArgument(limiter.tryAcquire(1), GlobalCallbackCode.RATE_LIMIT);
         return Boolean.TRUE;
     }
 
